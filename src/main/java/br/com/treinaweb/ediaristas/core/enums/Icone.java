621e498c8534bd1c5c7fe2f3417cd44ed907e7c8
MatheusGorga <matheusgobar@gmail.com>
2023-05-10 15:23:53 -0300

    inicioProjeto

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/enums/Icone.java
@@ -0,0 +1,25 @@
+package br.com.treinaweb.ediaristas.core.enums;
+
+public enum Icone {
+
+    TWF_CLEANING_1("twf-cleaning-1"),
+    TWF_CLEANING_2("twf-cleaning-2"),
+    TWF_CLEANING_3("twf-cleaning-3");
+
+    private String nome;
+
+    private Icone(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+
+}
---
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -0,0 +1,53 @@
+package br.com.treinaweb.ediaristas.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristas.core.enums.Icone;
+import br.com.treinaweb.ediaristas.core.models.Servico;
+import br.com.treinaweb.ediaristas.core.repositories.ServicoRepository;
+
+@Controller
+@RequestMapping("/admin/servicos")
+public class ServicoController {
+
+    @Autowired
+    private ServicoRepository repository;
+
+    @GetMapping
+    public ModelAndView buscarTodos() {
+        var modelAndView = new ModelAndView("admin/servico/lista");
+
+        modelAndView.addObject("servicos", repository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        var modelAndView = new ModelAndView("admin/servico/form");
+
+        modelAndView.addObject("servico", new Servico());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+
+        repository.save(servico);
+
+        return "redirect:/admin/servicos/lista";
+    }
+
+    @ModelAttribute("icones")
+    public Icone[] getIcones() {
+        return Icone.values();
+    }
+
+}
